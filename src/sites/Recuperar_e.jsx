bff713a58bb88d3ac268b8c42cec8595097044f0
criz707 <120576092+criz707@users.noreply.github.com>
2025-04-04 06:51:40 -0500

    finalizamos pantallas - Modulo de inicio de sesion
    finalizamos con las 3 pantallas de inicio de sesion

--- src/sites/Recuperar_e.jsx
+++ src/sites/Recuperar_e.jsx
@@ -4,14 +4,14 @@ import guy from "../assets/fallout_guy3.png"
 export const Recuperar_e = () => {
   const naviagate = useNavigate();
 
-  const goToMenu = () =>{
-    naviagate("/")  
+  const goToMenu = () => {
+    naviagate("/")
   }
   return (
     <div className="Container">
       <img src={guy} alt="" className="Imagen" />
       <p className="texto">
-      ¡Correo de recuperacion enviado a travol*****com!
+        ¡Correo de recuperacion enviado a travol*****com!
       </p>
       <button className="boton" onClick={goToMenu}>
         Volver
--- src/sites/Registro.jsx
+++ src/sites/Registro.jsx
@@ -2,48 +2,49 @@ import logo from "../assets/logo.png"
 import '../Styles/Desktop/Registro/Registro.css'
 import { Volver } from "../components/Volver"
 import { useNavigate } from "react-router-dom"
+import { Button } from "reactstrap"
 
 
 export const Registro = () => {
   const navigate = useNavigate();
-  const goToRegistroExitoso = () =>{
+  const goToRegistroExitoso = () => {
     navigate("")
   }
 
   return (
-    <div className = "inicio">
-        <Volver ruta={"/"}/>
+    <div className="inicio">
+      <Volver ruta={"/"} />
 
-        
-        <form className="sesion">
 
-          <p>Nombre:</p>
-          <input type="text" placeholder="nombre" id="nombre"/>
+      <form className="sesion">
 
-          <p>Correo:</p>
-          <input type="text" placeholder="correo" id="correo"/>
+        <p>Nombre:</p>
+        <input type="text" placeholder="nombre" id="nombre" />
 
-          <p>CC:</p>
-          <input type="number" placeholder="cc" id="cc"/>
+        <p>Correo:</p>
+        <input type="text" placeholder="correo" id="correo" />
 
-          <p>Contraseña:</p>
-          <input type="text" placeholder="contrasena" id="contrasena"/>
+        <p>CC:</p>
+        <input type="number" placeholder="cc" id="cc" />
 
-          
-          <button type="submit">
-            Registrarse
-          </button>
-          
-        </form>
+        <p>Contraseña:</p>
+        <input type="text" placeholder="contrasena" id="contrasena" />
 
-        <div className="imagen">
-            <div className="title">
-                !Bienvenido usuario nuevo! 😃
-            </div>
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
+
+        <Button type="submit">
+          Registrarse
+        </Button>
+
+      </form>
+
+      <div className="imagen">
+        <div className="title">
+          !Bienvenido usuario nuevo! 😃
         </div>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+      </div>
 
     </div>
   )
