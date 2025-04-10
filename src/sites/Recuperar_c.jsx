3bce8ee7210995cb097f1aaadfe7db2eb2df8fbf
criz707 <120576092+criz707@users.noreply.github.com>
2025-04-10 09:54:32 -0500

    Boostrap - implementacion en pantalla

--- src/sites/Recuperar_c.jsx
+++ src/sites/Recuperar_c.jsx
@@ -2,30 +2,32 @@ import guy from '../assets/Hombre_estrellado.png'
 import '../Styles/Desktop/Recuperar_c/Recuperar_c.css'
 import { useNavigate } from 'react-router-dom'
 import { Volver } from '../components/Volver'
+import { Button } from 'reactstrap'
+
 
 
 export const Recuperar_c = () => {
 
     const navigate = useNavigate();
-    const goToExito = () =>{
+    const goToExito = () => {
         navigate("/recuperar_c/recuperar_e")
     }
-  return (
-    <div className="container_recuperar">
-        <Volver ruta={"/inicio_sesion"}/>
+    return (
+        <div className="container_recuperar">
+            <Volver ruta={"/inicio_sesion"} />
 
-        <form action="" className="Form">
-            <label >Ingrese el correo de la cuenta
-            de la cual desea recuperarla contraseña:</label>
-            <input type="text" id='correo'/>
-            <button className="Enviar" onClick={goToExito}>
-                Enviar
-            </button>
-        </form>
-        <div className="imagen_recuperar">
-            <img src={guy} alt="" />
+            <form action="" className="Form">
+                <label >Ingrese el correo de la cuenta
+                    de la cual desea recuperarla contraseña:</label>
+                <input type="text" id='correo' />
+                <Button className="Enviar" onClick={goToExito}>
+                    Enviar
+                </Button>
+            </form>
+            <div className="imagen_recuperar">
+                <img src={guy} alt="" />
+            </div>
         </div>
-    </div>
-  )
+    )
 
 }
