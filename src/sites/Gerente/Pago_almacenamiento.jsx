df921eaa0006d61ae902ead1dd522e2b6d445d42
criz707 <120576092+criz707@users.noreply.github.com>
2025-04-10 09:34:26 -0500

    Boostrap - implementacion en sitio

--- src/sites/Gerente/Pago_almacenamiento.jsx
+++ src/sites/Gerente/Pago_almacenamiento.jsx
@@ -1,38 +1,40 @@
+import { Button } from 'reactstrap'
 import aws from '../../assets/aws.png'
 import factura from "../../assets/Iconos_perfil/factura.png"
 import { Volver } from '../../components/Volver'
 import '../../Styles/Desktop/gerente/Pago_almacenamiento/Pago_almacenamiento.css'
 
+
 export const Pago_almacenamiento = () => {
-    const goToPago = () =>{
+    const goToPago = () => {
         window.location.href = "https://www.americanexpress.com/";
     }
-  return (
-    <div className="pago_almacen">
-        <Volver ruta={"/gerente/configuraciones_g"}/>
+    return (
+        <div className="pago_almacen">
+            <Volver ruta={"/gerente/configuraciones_g"} />
 
-        <div className="body">
+            <div className="body">
 
-            <div className="target">
+                <div className="target">
 
-                <div className="imagen">
-                    <img src={factura} alt="" />
+                    <div className="imagen">
+                        <img src={factura} alt="" />
+                    </div>
+
+                    <div className="info">
+                        <h3>El consumo de este mes fue de: </h3>
+
+                        <img src={aws} alt="" />
+
+                        <p>Almacenamiento : $*****</p>
+
+                        <Button className="boton" onClick={goToPago}>
+                            Ir a pagar
+                        </Button>
+                    </div>
+
                 </div>
-
-                <div className="info">
-                    <h3>El consumo de este mes fue de: </h3>
-
-                    <img src={aws} alt="" />
-
-                    <p>Almacenamiento : $*****</p>
-
-                    <button className="boton" onClick={goToPago}>
-                        Ir a pagar
-                    </button>
-                </div>
-
             </div>
         </div>
-    </div>
-  )
+    )
 }
