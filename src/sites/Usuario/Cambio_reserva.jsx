deb307804ad3fef210100d56c06e6d9b30a02136
criz707 <120576092+criz707@users.noreply.github.com>
2025-04-10 09:17:49 -0500

    Boostrap - actualizacion de botones

--- src/sites/Usuario/Cambio_reserva.jsx
+++ src/sites/Usuario/Cambio_reserva.jsx
@@ -1,56 +1,58 @@
-import {Volver} from "../../components/Volver"
+import { Volver } from "../../components/Volver"
 import foto_mesa from '../../assets/fondo_mesa.png'
 import '../../Styles/Desktop/usuario/Cambio_reserva/Cambio_reserva.css'
 import { useNavigate } from "react-router-dom"
+import { Button } from "reactstrap"
+
 
 export const Cambio_reserva = () => {
     const navigate = useNavigate();
-    const goToReservaExistosa = () =>{
+    const goToReservaExistosa = () => {
         navigate("/usuario/cambio_reserva/reserva_exitosa")
     }
 
-    const goToReservaFallida = () =>{
+    const goToReservaFallida = () => {
         navigate("/usuario/error_reserva")
     }
 
-    const goToPenalizacion = () =>{
+    const goToPenalizacion = () => {
         navigate("/usuario/penalizacion")
     }
 
-  return (
-    <div className="cambio_reserva">
+    return (
+        <div className="cambio_reserva">
 
 
-        <Volver ruta={"/usuario/mesas_reserva"}/>
+            <Volver ruta={"/usuario/mesas_reserva"} />
 
-        <div className="body">
-            <div className="horario">
+            <div className="body">
+                <div className="horario">
 
-                <div className="titulo" onClick={goToPenalizacion}>Mesa 4</div>
-                <div className="plazas">
-                    <div className="circle"></div>
-                    <div className="titulo" >Plazas disponibles</div>
-                    {/* Select del usuario */}
-                    <select name="" id="">
-                        <option className='ok_option' value="7-9">7 pm - 9 pm</option>
-                        <option className='ok_option' value="7-9">7 pm - 9 pm</option>
-                        <option className='ok_option'value="7-9">7 pm - 9 pm</option>
-                        <option className='ok_option' value="7-9">7 pm - 9 pm</option>
-                        <option className='no_option' value="7-9">7 pm - 9 pm</option>
-                    </select>
+                    <div className="titulo" onClick={goToPenalizacion}>Mesa 4</div>
+                    <div className="plazas">
+                        <div className="circle"></div>
+                        <div className="titulo" >Plazas disponibles</div>
+                        {/* Select del usuario */}
+                        <select name="" id="">
+                            <option className='ok_option' value="7-9">7 pm - 9 pm</option>
+                            <option className='ok_option' value="7-9">7 pm - 9 pm</option>
+                            <option className='ok_option' value="7-9">7 pm - 9 pm</option>
+                            <option className='ok_option' value="7-9">7 pm - 9 pm</option>
+                            <option className='no_option' value="7-9">7 pm - 9 pm</option>
+                        </select>
+                    </div>
+                    <Button className="boton" onClick={goToReservaExistosa}>
+                        Reservar
+                    </Button>
+
                 </div>
-                <button className="boton" onClick={goToReservaExistosa}>
-                Reservar
-                </button>
 
-            </div>
 
-            
 
-            <div className="imagen">
-            <img src={foto_mesa} alt="" onClick={goToReservaFallida} />
+                <div className="imagen">
+                    <img src={foto_mesa} alt="" onClick={goToReservaFallida} />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
