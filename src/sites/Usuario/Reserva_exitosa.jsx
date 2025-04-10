60b4c7ec82a3c336a38e568101d43c473515a72a
criz707 <120576092+criz707@users.noreply.github.com>
2025-04-10 09:29:09 -0500

    Boostrap - actualizacion de botones

--- src/sites/Usuario/Reserva_exitosa.jsx
+++ src/sites/Usuario/Reserva_exitosa.jsx
@@ -2,40 +2,41 @@ import { Volver } from "../../components/Volver"
 import mesa from "../../assets/mesa.jpg"
 import '../../Styles/Desktop/usuario/Reserva_exitosa/Reserva_exitosa.css'
 import { useNavigate } from "react-router-dom"
+import { Button } from "reactstrap"
 
 
 
 export const Reserva_exitosa = () => {
     const navigate = useNavigate();
-    const goToMenu = () =>{
+    const goToMenu = () => {
         navigate("/usuario/mesas_reserva");
     }
-  return (
-    <div className="reserva_exitosa">
+    return (
+        <div className="reserva_exitosa">
 
-        <Volver ruta={"/usuario/mesas_reserva"}/>
+            <Volver ruta={"/usuario/mesas_reserva"} />
 
-        <div className="body">
+            <div className="body">
 
-            <div className="info">
+                <div className="info">
 
-                <li>Mesa para el 07 de marzoa las 2:00 pm - 4:00 p.m</li>
-                <li>Llegara un correo con al informacion requerida en unos minutos</li>
+                    <li>Mesa para el 07 de marzoa las 2:00 pm - 4:00 p.m</li>
+                    <li>Llegara un correo con al informacion requerida en unos minutos</li>
 
-                <button className="boton" onClick={goToMenu}>
-                    Volver al menu principal
-                </button>
+                    <Button className="boton" onClick={goToMenu}>
+                        Volver al menu principal
+                    </Button>
+
+                </div>
+
+                <div className="imagen">
+                    <p>! Reserva realizada con exito ¡</p>
+                    <img src={mesa} alt="" />
+                </div>
 
             </div>
 
-            <div className="imagen">
-                <p>! Reserva realizada con exito ¡</p>
-                <img src={mesa} alt="" />
-            </div>
-
-        </div>        
-
-    </div>
-  )
+        </div>
+    )
 }
 
